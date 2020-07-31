e4c5f42b78023c95a5f86eb7a6c0c041727707ba
Harry Carpenter <harrycarpenter@ozoneteam.net>
2020-07-31 14:13:33 +0800

    Lab 01 Q 1-5 Done

---
+++ Lab-01/cmake-build-debug/CMakeFiles/Lab_01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_01.dir/rotate.c.o"
+  "Lab_01.pdb"
+  "Lab_01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
